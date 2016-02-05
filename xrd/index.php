e393626f14c539b8762f2157ef2dbc3d98c53338
Chimo <chimo@chromic.org>
2016-02-05 14:24:13 -0500

    Fixup XRD stuff
    * Keep the 'secret' secret by putting it in a gitignored config file.
    * Delete XML files using known secret
    * well-known should use HTTPS to point to XRD

--- xrd/index.php
+++ xrd/index.php
@@ -1,27 +1,8 @@
 <?php
 
-/*
- * GNU social
- * Copyright (C) 2010, Free Software Foundation, Inc
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+require_once('../../private/_config.php');
 
-
-$s = "5Oz2jrBTOskPs6KXAl7j";
-
-/* this should be a secret */
+$s = $config['xrd']['secret'];
 
 $u = $_GET['uri'];
 
@@ -36,6 +17,3 @@ if (file_exists($f)) {
   header('Content-type: text/xml');
   echo $c;
 }
-
-
-?>
